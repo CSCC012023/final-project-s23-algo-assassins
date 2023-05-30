0deca4fd9f806390bcd48e05a542d814f1eb9fab
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2023-05-30 16:22:51 -0400

    Added signup screen

---
+++ front-end/src/screens/SignUpScreen.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SignUpScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default SignUpScreen;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const styles = StyleSheet.create({});
