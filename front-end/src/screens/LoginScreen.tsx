7b91d478fe4994f22c004ba3c826aaba7f46b782
Henry <44884792+hoomji@users.noreply.github.com>
2023-05-30 15:45:30 -0400

    init login screen

---
+++ front-end/src/screens/LoginScreen.tsx
@@ -0,0 +1,15 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+
+const LoginScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
