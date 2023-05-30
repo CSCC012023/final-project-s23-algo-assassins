f9ab1f06d34f86fbb326aa51dc99998f2bfc0ce7
Henry <44884792+hoomji@users.noreply.github.com>
2023-05-30 16:56:20 -0400

    Added back button to login

--- front-end/src/screens/LoginScreen.tsx
+++ front-end/src/screens/LoginScreen.tsx
@@ -1,11 +1,15 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const LoginScreen = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
+      <Icon name="arrow-back-ios" size={30}></Icon>
+      {/* <Icon.Button name="facebook" backgroundColor="#3b5998">
+        Login with Facebook
+      </Icon.Button> */}
     </View>
   );
 };
