2fa29d4a17da884c898b56df2568d0b0de4c5d73
Henry <44884792+hoomji@users.noreply.github.com>
2023-06-26 21:41:38 -0400

    Update welcomeCard styles

--- front-end/src/components/welcomeCard/WelcomeCard.tsx
+++ front-end/src/components/welcomeCard/WelcomeCard.tsx
@@ -5,13 +5,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const WelcomeCard = () => (
   <Card>
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'baseline',
-        marginBottom: 20,
-      }}>
+    <View style={styles.cardContainer}>
       <Text>
         Welcome to fitbook, {/* Text bold */}
         <Text
@@ -31,25 +25,19 @@ const WelcomeCard = () => (
     <Text>
       Feel free to follow the fitbook team below for exciting updates and more!
     </Text>
-    <View
-      style={{
-        flexDirection: 'row',
-        marginTop: 25,
-        alignSelf: 'center',
-        marginBottom: 20,
-      }}>
+    <View style={styles.followContainer}>
       <Image
         source={require('../../assets/images/FitBook_logo2.png')}
-        style={{width: 60, height: 75, marginRight: 15}}
+        style={styles.logo}
       />
       <View>
-        <View style={{marginBottom: 10}}>
-          <Text h4 style={{fontWeight: 'bold'}}>
+        <View style={styles.fitbookTeam}>
+          <Text h4 style={styles.bold}>
             Fitbook Team
           </Text>
         </View>
         <Button
-          containerStyle={{height: 35}}
+          containerStyle={styles.buttonContainer}
           titleProps={{
             style: {
               fontSize: 15,
@@ -70,4 +58,21 @@ const styles = StyleSheet.create({
   closeIcon: {
     fontSize: 35,
   },
+  cardContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+    marginBottom: 20,
+  },
+
+  followContainer: {
+    flexDirection: 'row',
+    marginTop: 25,
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  logo: {width: 60, height: 75, marginRight: 15},
+  fitbookTeam: {marginBottom: 10},
+  bold: {fontWeight: 'bold'},
+  buttonContainer: {height: 35},
 });
