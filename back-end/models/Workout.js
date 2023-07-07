ae20a2f95238a3f5d1888424ec7347b841e46f36
Yu Yang Chen <kennyyuyang.chen@mail.utoronto.ca>
2023-07-06 22:12:31 -0400

    user backend support

--- back-end/models/Workout.js
+++ back-end/models/Workout.js
@@ -7,7 +7,9 @@ exports.Workout = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const Exercise = require('Exercise.ts');
 const WorkoutSchema = new mongoose_1.default.Schema({
+    _id: mongoose_1.default.Schema.Types.ObjectId,
     name: { type: String, required: true },
+    description: { type: String },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
     exercises: [Exercise]
--- back-end/models/Workout.ts
+++ back-end/models/Workout.ts
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 const Exercise = require('Exercise.ts');
 
 const WorkoutSchema = new mongoose.Schema ({
+    _id: mongoose.Schema.Types.ObjectId,
+    userEmail: String,
     name: { type: String, required: true },
+    description: { type: String },
+    duration: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-    exercises: [Exercise]
-})
+    exercises: [ Exercise ]
+});
 
 export const Workout = mongoose.model("Workout", WorkoutSchema);
 
