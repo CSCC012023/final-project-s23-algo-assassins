55127cd5aebf547f4205409c64a6370b072e0895
Jeremy Cheng <81793294+Jeremy-Cheng-stack@users.noreply.github.com>
2023-07-27 13:32:56 -0400

    Update user_router.ts

--- back-end/routers/user_router.ts
+++ back-end/routers/user_router.ts
@@ -339,6 +339,10 @@ userRouter.patch("/create/follow", async (req, res) => {
     res.status(400).json({ message: "A user is not found" });
     return;
   }
+  if (user1.followers.includes(user2.email)) {
+    res.status(400).json({ message: "User already followed" });
+    return;
+  }
   user1.followers.push(user2.email);
   user2.following.push(user1.email);
 
